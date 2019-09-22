d61a363d723bb6b31d707e0994ce48efc14c49bb
s-parvizi <sohail.parvizi@gmail.com>
2019-09-22 18:20:16 +0330

    Add single component counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import { throwStatement } from "@babel/types";
+import { runInThisContext } from "vm";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0
+  };
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement("clicked")}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount = () => {
+    const { count } = this.state;
+    const x = <h1>Zero</h1>;
+    return count === 0 ? x : count;
+  };
 }
 
 export default Counter;
