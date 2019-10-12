569334786c86a0953ff550be3fe514b359357802
s-parvizi <sohail.parvizi@gmail.com>
2019-10-12 16:41:01 +0330

    Add counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,17 +4,17 @@ import { runInThisContext } from "vm";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
   handleIncrement = product => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement("clicked")}
@@ -22,18 +22,18 @@ class Counter extends Component {
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount = () => {
-    const { count } = this.state;
+    const { value: count } = this.state;
     const x = <h1>Zero</h1>;
     return count === 0 ? x : count;
   };
